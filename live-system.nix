a6f579817b4c606ee76d62a30c6971fbbca277ad
Florian Franzen <florian@gli.al>
2023-06-28 15:54:59 +0200

    iso: update iso and add additional utils

--- live-system.nix
+++ live-system.nix
@@ -1,18 +1,39 @@
 { pkgs, lib, modulesPath, ... }:
 
 {
-  imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
+  imports = [
+    (modulesPath + "/installer/cd-dvd/installation-cd-base.nix")
+    (modulesPath + "/profiles/minimal.nix")
+  ];
 
+  # Hardened kernel might help
   boot.kernelPackages = pkgs.linuxPackages_hardened;
 
+  # Make sure iso is distinguishable
   isoImage.edition = lib.mkForce "mindthegap";
   networking.hostName = "mind-the-gap";
 
+  # Add services and udev rules for common smartcards
   services.pcscd.enable = true;
-  services.udev.packages = [ pkgs.yubikey-personalization ];
 
-  environment.systemPackages = with pkgs; [ mind-the-gap gnupg paperkey wget yubikey-manager pwgen xkcdpass ];
+  hardware.gpgSmartcards.enable = true;
+  hardware.nitrokey.enable = true;
 
+  services.udev.packages = [ pkgs.yubikey-personalization pkgs.solo2-cli ];
+
+  # Add any tools we might need
+  environment.systemPackages = with pkgs; [
+    mind-the-gap
+    gnupg
+    paperkey
+    pwgen
+    solo2-cli
+    wget
+    xkcdpass
+    yubikey-manager
+  ];
+
+  # Enable gnupg ssh agent
   programs = {
     ssh.startAgent = false;
     gnupg.agent = {
