d66a5bbed7df24f6cbe4b93d0d11373bd8267567
Florian Franzen <florian@gli.al>
2023-10-02 11:53:40 +0200

    man: include subcommand pages

--- src/bin/mind-the-build.rs
+++ src/bin/mind-the-build.rs
@@ -1,7 +1,9 @@
-use clap::{Parser, ValueEnum};
+use std::path::PathBuf;
 
-use clap_mangen::Man;
+use clap::{Command, Parser, ValueEnum};
+
 use clap_complete::Shell;
+use clap_mangen::Man;
 
 #[derive(Parser, Debug)]
 #[command(author, version)]
@@ -10,27 +12,48 @@ struct OutputConfig {
     prefix: std::path::PathBuf,
 }
 
+/// Helper command to easily write manpage of command to directory
+fn generate_man_to(cmd: &Command, dir: PathBuf) -> std::io::Result<()> {
+    let name = cmd.get_display_name().unwrap_or_else(|| cmd.get_name());
+    let mut file = std::fs::File::create(dir.join(format!("{name}.1")))?;
+
+    let man = Man::new(cmd.clone());
+    man.render(&mut file)?;
+
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
     // Parse command lone
     let OutputConfig { prefix } = OutputConfig::parse();
 
     // Build command line interface
     let mut cmd = mind_the_gap::cli::command();
-    let name = cmd.get_name().to_string();
+    cmd.build();
 
-    // Export manpage
-    let man = Man::new(cmd.clone());
-    let mut buffer: Vec<u8> = Default::default();
-    man.render(&mut buffer)?;
-
+    // Export manpage for ...
     let mandir = prefix.join("share/man/man1");
 
     println!("Exporting manpage to '{}'...", mandir.display());
 
     std::fs::create_dir_all(mandir.clone())?;
-    std::fs::write(mandir.join(format!("{name}.1")), buffer)?;
+
+    // - Main executable ...
+    generate_man_to(&cmd, mandir.clone())?;
+
+    // - ... and each backend ...
+    for subcmd in cmd.get_subcommands() {
+        generate_man_to(subcmd, mandir.clone())?;
+
+        // - ... and each of the backend's commands
+        for subsubcmd in subcmd.get_subcommands() {
+            generate_man_to(subsubcmd, mandir.clone())?;
+        }
+    }
 
     // Export shell completions
+    let name = cmd.get_name().to_string();
+
     for &shell in Shell::value_variants() {
         let suffix = match shell {
             Shell::Bash => Some("share/bash-completion/completions"),
